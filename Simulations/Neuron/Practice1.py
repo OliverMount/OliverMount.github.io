b6590ed1bc9386ea5f3979df77f5c3bde990c433
OliverMount <oliverjmount@yahoo.com>
2025-02-12 14:39:42 +0900

    updated

--- Simulations/Neuron/Practice1.py
+++ Simulations/Neuron/Practice1.py
@@ -93,72 +93,79 @@ ax.spines[['top','right']].set_visible(False)
 ax.legend(title='Stim. Amp (nA)', title_fontsize=16, frameon=False) 
 plt.show()
 
- 
-# Why the below is differnt from above? 
-fig, ax = plt.subplots()
-amps = [0.02,0.05,0.075,0.1,0.15]
-colors = ["cyan","green", "blue", "red", "black"]
-labels=[str(round(k,4)) for k in amps]
-for amp, color,label in zip(amps, colors,labels):
-    stim.amp = amp
-    h.finitialize(-65 * mV)
-    h.continuerun(25 * ms)
-    ax.plot(t, list(soma_v), color=color, label=label) 
-    
-ax.set_xlabel("Time (ms)",fontsize=20)
-ax.set_ylabel("Voltage (mV)",fontsize=20)
-ax.tick_params(axis='both', labelsize=16) 
-ax.spines[['top','right']].set_visible(False) 
-ax.legend(title='Stim. Amp (nA)', title_fontsize=16, frameon=False) 
-plt.show()
-
-
-# Tutorial example along with dendritic current  
+  
+# Tutorial example along with dendritic current   
 amps = [0.075 * i for i in range(1, 5)]  # [0.075, 0.15, 0.225, 0.3]
 colors = ["green", "blue", "red", "black"]
-labels = [round(k,4) for k in amps] 
-
-fig, ax = plt.subplots()  # Create a figure and axis using plt.subplots
-
+labels = [round(k, 4) for k in amps] 
+fig, ax = plt.subplots(figsize=(10, 6))  # Create a figure and axis using plt.subplots 
 for amp, color, label in zip(amps, colors, labels):
     stim.amp = amp
     h.finitialize(-65 * mV)
     h.continuerun(25 * ms)
-    ax.plot(t, list(soma_v), color=color, label=label)  # Use ax.plot instead of plt.plot 
-    ax.plot(t,list(dend_v),color=color,linestyle="--") 
-    
+    ax.plot(t, list(soma_v), color=color, label= label )
+    ax.plot(t, list(dend_v), color=color, linestyle="--")
 
 # Create a separate legend for soma and dendritic currents
 custom_lines = [
     Line2D([0], [0], color="black", linestyle="-", label="Soma Voltage"),
     Line2D([0], [0], color="black", linestyle="--", label="Dendritic Voltage")
 ]
-ax.legend(handles=custom_lines,   fontsize=14, title="Signal Type", title_fontsize=16)
- 
-ax.set_xlabel("Time (ms)",fontsize=20)
-ax.set_ylabel("Voltage (mV)",fontsize=20)
-ax.tick_params(axis='both', labelsize=16) 
-ax.spines[['top','right']].set_visible(False) 
-amp_legend = ax.legend(title='Stim. Amp (nA)', title_fontsize=16, frameon=False, fontsize=14)
-ax.add_artist(amp_legend) 
+
+# Add the amplitude legend
+first_legend = ax.legend(title='Stim. Amp (nA)', title_fontsize=16, fontsize=16, loc='upper right',frameon=False,bbox_to_anchor=(0.8, 1.0)) 
+# Add the custom legend for signal types
+ax.add_artist(first_legend)
+ax.legend(handles=custom_lines, title="Signal Type",fontsize=16,  title_fontsize=16, loc='upper right',frameon=False,bbox_to_anchor=(0.85, 0.5))
+
+ax.set_xlabel("Time (ms)", fontsize=20)
+ax.set_ylabel("Voltage (mV)", fontsize=20)
+ax.tick_params(axis='both', labelsize=16)
+ax.spines[['top', 'right']].set_visible(False)
+
+plt.tight_layout()
 plt.show()
- 
-# The dendritic currnet is different here 
+
+
+
+# Why the below is differnt from above?  
 amps = [0.02,0.05,0.075,0.1,0.15]
-colors = ["gray","green", "blue", "red", "black"]
-for amp, color in zip(amps, colors):
+colors = ["cyan","green", "blue", "red", "black"]
+labels=[str(round(k,4)) for k in amps]
+
+fig, ax = plt.subplots(figsize=(10, 6))  # Create a figure and axis using plt.subplots 
+for amp, color, label in zip(amps, colors, labels):
     stim.amp = amp
     h.finitialize(-65 * mV)
     h.continuerun(25 * ms)
-    plt.plot(t,list(soma_v),color=color) 
-    plt.plot(t,list(dend_v),color=color,linestyle="--") 
+    ax.plot(t, list(soma_v), color=color, label= label )
+    ax.plot(t, list(dend_v), color=color, linestyle="--")
+
+# Create a separate legend for soma and dendritic currents
+custom_lines = [
+    Line2D([0], [0], color="black", linestyle="-", label="Soma Voltage"),
+    Line2D([0], [0], color="black", linestyle="--", label="Dendritic Voltage")
+]
+
+# Add the amplitude legend
+first_legend = ax.legend(title='Stim. Amp (nA)', title_fontsize=16, fontsize=16, loc='upper right',frameon=False,bbox_to_anchor=(0.8, 1.0)) 
+# Add the custom legend for signal types
+ax.add_artist(first_legend)
+ax.legend(handles=custom_lines, title="Signal Type",fontsize=16,  title_fontsize=16, loc='upper right',frameon=False,bbox_to_anchor=(0.85, 0.5))
+
+ax.set_xlabel("Time (ms)", fontsize=20)
+ax.set_ylabel("Voltage (mV)", fontsize=20)
+ax.tick_params(axis='both', labelsize=16)
+ax.spines[['top', 'right']].set_visible(False)
+
+plt.tight_layout()
 plt.show()
+
+  
 
 
 ## Role of number of segments (nseg) 
-
-import bokeh.plotting as plt
-
+ 
 f = plt.figure(x_axis_label="t (ms)", y_axis_label="v (mV)")
 amps = [0.075 * i for i in range(1, 5)]
 colors = ["green", "blue", "red", "black"]
