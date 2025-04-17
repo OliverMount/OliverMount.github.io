c0b218f15810d9588f7cc6289e3e6266ce12fb7b
OliverMount <oliverjmount@yahoo.com>
2025-04-17 17:20:22 +0900

    updated

--- ver2.sh
+++ ver2.sh
@@ -35,10 +35,12 @@ echo -e "##############\n"
 
 # Set environment variables
 module purge   
-module load gcc/12.2.0 pgi/23.5 bison/3.8.2 flex/2.6.4 cmake/3.28.1 python/.3.12.3
 
 module load /opt/ibs_lib/modulefiles/libraries/.cuda/25.1
 module load /opt/ibs_lib/apps/nvhpc/25.1/modulefiles/nvhpc/25.1
+
+module load gcc/12.2.0 pgi/23.5 bison/3.8.2 flex/2.6.4 cmake/3.28.1 python/.3.12.3
+
 
 export MPI_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5
 export PATH=$MPI_HOME/bin:$PATH
