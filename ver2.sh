e3841c76da448d850ab88a42bb7b6f1e4b8bda07
OliverMount <oliverjmount@yahoo.com>
2025-04-17 18:07:03 +0900

    updated

--- ver2.sh
+++ ver2.sh
@@ -43,9 +43,9 @@ export CUDA_HOME=/opt/ibs_lib/apps/cudatoolkit/cuda-12.2
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
-export NVHPC_CUDA_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/cuda
-export PATH=$NVHPC_CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$NVHPC_CUDA_HOME/lib64:$LD_LIBRARY_PATH
+#export NVHPC_CUDA_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/cuda
+#export PATH=$NVHPC_CUDA_HOME/bin:$PATH
+#export LD_LIBRARY_PATH=$NVHPC_CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 #export CUDA_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/cuda
 #export PATH=$CUDA_HOME/bin:$PATH
