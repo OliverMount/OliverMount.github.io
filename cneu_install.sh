26471f378905700c7dba3cd99c4c77b7f0a546fb
OliverMount <oliverjmount@yahoo.com>
2025-04-17 17:01:37 +0900

    updated

--- cneu_install.sh
+++ cneu_install.sh
@@ -39,9 +39,13 @@ module load gcc/12.2.0 pgi/23.5 bison/3.8.2 flex/2.6.4 cmake/3.28.1 python/.3.12
 module load /opt/ibs_lib/modulefiles/libraries/.cuda/25.1
 module load /opt/ibs_lib/apps/nvhpc/25.1/modulefiles/nvhpc/25.1
 
-export MPI_CXX_COMPILER=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/mpi/bin/mpicxx
-export MPI_CXX_INCLUDE_PATH=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5/include/
-export MPI_CXX_LIBRARIES=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5/lib/libmpi.so
+export MPI_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5
+export PATH=$MPI_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$MPI_HOME/lib:$LD_LIBRARY_PATH
+
+#export MPI_CXX_COMPILER=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/mpi/bin/mpicxx
+#export MPI_CXX_INCLUDE_PATH=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5/include/
+#export MPI_CXX_LIBRARIES=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5/lib/libmpi.so
 
 export CMAKE_INCLUDE_PATH=/opt/ibs_lib/apps/readline/8.2/include:$CMAKE_INCLUDE_PATH
 export CMAKE_LIBRARY_PATH=/opt/ibs_lib/apps/readline/8.2/lib:$CMAKE_LIBRARY_PATH
