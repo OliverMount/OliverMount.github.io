c1815e8158376a8a590d307d91d867496bf2a11a
OliverMount <oliverjmount@yahoo.com>
2025-04-18 16:06:45 +0900

    updated

--- cneu_install.sh
+++ cneu_install.sh
@@ -36,19 +36,19 @@ echo -e "##############\n"
 # Set environment variables
 module purge   
 #module load /opt/ibs_lib/modulefiles/libraries/.cuda/25.1
+export NVHPC_CUDA_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/cuda/12.6/
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+#export C_INCLUDE_PATH=$CUDA_HOME/include:$C_INCLUDE_PATH  #
+
 module load /opt/ibs_lib/apps/nvhpc/25.1/modulefiles/nvhpc/25.1
-
-export CUDA_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/cuda/12.6/
-
-
-
 
 module load gcc/12.2.0 pgi/23.5 bison/3.8.2 flex/2.6.4 cmake/3.28.1 python/.3.12.3
 #Read lines
 export CMAKE_INCLUDE_PATH=/opt/ibs_lib/apps/readline/8.2/include:$CMAKE_INCLUDE_PATH
 export CMAKE_LIBRARY_PATH=/opt/ibs_lib/apps/readline/8.2/lib:$CMAKE_LIBRARY_PATH
 
-
+# LOADING MPI seperately
 export MPI_HOME=/opt/ibs_lib/apps/nvhpc/25.1/Linux_x86_64/25.1/comm_libs/12.6/openmpi4/openmpi-4.1.5
 export PATH=$MPI_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$MPI_HOME/lib:$LD_LIBRARY_PATH
