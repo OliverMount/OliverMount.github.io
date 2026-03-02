b291e2e7b912ec0774500dab6e392bdb4bd9652a
OliverMount <oliverjmount@yahoo.com>
2026-03-02 11:06:06 +0900

    updated

--- extra.R
+++
@@ -1,8 +0,0 @@
-#- text: "Fundamentals"
-#  file: Simulations/Neuron/Fundamentals.qmd 
-
-
-#reticulate::repl_python()
-#reticulate::py_config()
-
-#Sys.setenv(RETICULATE_PYTHON = "/media/oli/Research/env_python/spy/bin/")
